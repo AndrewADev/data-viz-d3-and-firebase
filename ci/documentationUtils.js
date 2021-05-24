fa5417d88fa4730c39d75cccf9b0530d99524f1d
Andrew A <andrewadev@gmail.com>
2021-05-24 17:01:49 +0200

    Only move the preview files if different
    Only move the generated preview files if they are different from what is already present.
    
    For this, we leverage looks-same, for which we take the default configuration for color difference for now.

--- ci/documentationUtils.js
+++ ci/documentationUtils.js
@@ -1,6 +1,8 @@
 const fs = require("fs").promises;
 const path = require("path");
 
+const looksSame = require("looks-same");
+
 const fileNames = [
   "preview-hierarchy-example.png",
   "preview-project-one.png",
@@ -12,7 +14,19 @@ const moveFiles = async (oldLocation, newLocation) => {
   fileNames.forEach(async fileName => {
     const oldFull = path.join(oldLocation, fileName);
     const newFull = path.join(newLocation, fileName);
-    await fs.rename(oldFull, newFull);
+    looksSame(oldFull, newFull, async (err, { equal } = {}) => {
+      if (err) {
+        console.error("Error running comparison:", err);
+      }
+      if (equal) {
+        console.info(
+          `The file ${fileName} does not appear to have changed. It has not been moved`
+        );
+      } else {
+        await fs.rename(oldFull, newFull);
+        console.info(`The file ${fileName} has been updated`);
+      }
+    });
   });
 };
 
