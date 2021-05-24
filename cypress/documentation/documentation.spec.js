9aadac761118dcd7dd9d428e0106b34f09d3b98c
Andrew A <andrewadev@gmail.com>
2021-05-24 19:47:07 +0200

    Pseudo guard clause for donut chart
    Hack together a pseudo guard clause for making sure the donut chart has finished rendering before we take a screenshot.
    
    Note that we use actual guard clauses, but rely on knowing what height & width _should be_ to indicate it is nearly finished (for now).
    
    The tricky part that is not currently solved is that there is still a tiny bit of animation left at the time the parent reaches max width (filling in wedge gaps).
    
    This is clearly fragile and won't hold forever, but it is lightning fast compared to arbitrary waits.

--- cypress/documentation/documentation.spec.js
+++ cypress/documentation/documentation.spec.js
@@ -6,40 +6,54 @@ context('Misc', () => {
       capture: 'viewport',
       clip: { x: 0, y: 0, width: 1080, height: 700 },
       scale: false,
-      disableTimersAndAnimations: true,
       screenshotOnRunFailure: true,
-      beforeScreenshot () { },
-      afterScreenshot () { },
-    })
-  })
+      beforeScreenshot() {},
+      afterScreenshot() {}
+    });
+  });
 
   it('captures screenshot for project one ', () => {
+    cy.visit('/projects/one');
 
-    cy.visit('/projects/one')
-    // TODO: Can we wait on visual directly?
-    cy.get('.project-header').wait(1200).screenshot('preview-project-one')
-  })
+    // Initial request can sometimes take some time
+    cy.get('#chart-group', { timeout: 10000 })
+      .invoke('width')
+      // Not great in that we have to know the styling,
+      // but this gets us 95% of the way without an arbitrary
+      // wait. Unfortunateley, it is still subject to variance
+      // based on *how close* the animation is to finishing,
+      // as both width and height stabilize before the animation
+      // fully finishes (wedge-shaped gaps still closing between
+      // the sections)
+      // TODO: More reliable?
+      .should('be.gte', 300);
+
+    cy.get('.project-header').screenshot('preview-project-one');
+  });
 
   it('captures screenshot for project two ', () => {
-
-    cy.visit('/projects/two')
+    cy.visit('/projects/two');
     // TODO: Can we wait on visual directly?
-    cy.get('.project-background').wait(1200).screenshot('preview-project-two')
-  })
+    cy.get('.project-background')
+      .wait(1200)
+      .screenshot('preview-project-two');
+  });
 
   //http://localhost:8080/projects/hierarchy
 
   it('captures screenshot for hierarchy example', () => {
-
-    cy.visit('/projects/hierarchy')
+    cy.visit('/projects/hierarchy');
     // TODO: Can we wait on visual directly?
-    cy.get('.project-background').wait(1200).screenshot('preview-hierarchy-example')
-  })
+    cy.get('.project-background')
+      .wait(1200)
+      .screenshot('preview-hierarchy-example');
+  });
 
   it('captures screenshot for project three ', () => {
-
-    cy.visit('/projects/three')
+    cy.visit('/projects/three');
     // TODO: Can we wait on visual directly?
-    cy.get('.project-background').wait(1200).screenshot('preview-project-three')
-  })
-})
+    cy.get('.project-background')
+      .wait(1200)
+      .screenshot('preview-project-three');
+  });
+});
