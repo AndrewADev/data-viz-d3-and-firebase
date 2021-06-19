8542f8a083bc5e152c761bac07088c2828f89626
Andrew A <andrewadev@gmail.com>
2021-06-19 21:06:21 +0200

    Allow Firebase to auto-detect long-polling
    Adding an experimental setting for Firebase SDK to auto-detect long-polling.
    
    This would seem to be needed due to Cypress, which is effectively throttling requests to Firebase, compared to what it would be expecting. See answer here: https://stackoverflow.com/a/61619639
    
    It appears as well that there is an open issue for this on Cypress (2347)

--- src/firestore/index.js
+++ src/firestore/index.js
@@ -6,6 +6,11 @@ export const getDatabase = () => {
 
   if (process.env.VUE_APP_FIREBASE_HOST === 'emulator') {
     console.info('Connecting to Firebase emulator...')
+    // Needed due to Cypress, in order to keep communication open to Firebase with
+    // Cypress in the middle, per first comment here: https://stackoverflow.com/a/61619639
+    // Documentation on setting available here:
+    //   https://firebase.google.com/docs/reference/js/firebase.firestore.Settings#optional-experimentalautodetectlongpolling
+    db.settings({ experimentalAutoDetectLongPolling: true })
     db.useEmulator('localhost', 8050)
   }
   return db
