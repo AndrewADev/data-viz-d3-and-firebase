3220462ad13834f03eaffd83612f477da1d3970d
Andrew A <andrewadev@gmail.com>
2021-06-19 21:14:41 +0200

    Adding an initial e2e test for ProjectOne
    Adding our first real e2e test for ProjectOne.
    
    It still assumes some emulator seed data, which is forthcoming, but it navigates to Project One and then both adds and deletes a budget item, which is the main functionality for now.

---
+++ cypress/integration/ProjectOne.spec.js
@@ -0,0 +1,50 @@
+/// <reference types="cypress" />
+
+context('ProjectOne', () => {
+  it('successfully adds and removes BudgetItems', () => {
+    cy.visit('/')
+
+    // Navigate as the user would (tests that path too, prevents accidental deep-link-only)
+    cy.contains('Projects').click()
+    cy.contains('Project 1').click()
+
+    // A bit of waiting may be needed for initial data load
+    cy.get('#chart-group', { timeout: 10000 })
+      .find('path')
+      .should('have.length', 4)
+
+    cy.get('#legend-group')
+      .find('.cell')
+      .should('have.length', 4)
+
+    cy.get('#name').type('Oats')
+
+    cy.get('#cost')
+      .clear()
+      .type(60)
+
+    cy.get('button').click()
+
+    cy.get('#chart-group')
+      .find('path')
+      .should('have.length', 5)
+
+    cy.get('#legend-group')
+      .find('.cell')
+      .should('have.length', 5)
+
+    cy.get('#chart-group')
+      .find('path')
+      // Seems to be the most recently added item
+      .last()
+      .click()
+
+    cy.get('#chart-group')
+      .find('path')
+      .should('have.length', 4)
+
+    cy.get('#legend-group')
+      .find('.cell')
+      .should('have.length', 4)
+  })
+})
