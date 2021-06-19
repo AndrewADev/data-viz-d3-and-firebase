c493aaaac4ed55227599d69352755c261f596f02
Andrew A <andrewadev@gmail.com>
2021-06-19 18:47:11 +0200

    Allow using emulator for firebase in app
    For now we accomplish this with a flag prop that a caller can pass in.

--- src/firestore/index.js
+++ src/firestore/index.js
@@ -1,6 +1,10 @@
 import firebase from 'firebase/app'
 import 'firebase/firestore'
 
-export const getDatabase = () => {
-  return firebase.firestore()
+export const getDatabase = ({ useEmulator = false } = {}) => {
+  const db = firebase.firestore()
+  if (useEmulator) {
+    db.useEmulator('localhost', 8050)
+  }
+  return db
 }
