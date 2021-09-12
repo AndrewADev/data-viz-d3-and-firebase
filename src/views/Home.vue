354d66558c79e976070159016e5f470666930149
Andrew A <andrewadev@gmail.com>
2021-09-12 18:07:40 +0200

    Migrate away from Bootstrap-Vue
    Remive external components.
    
    Also remove some apparently empty markup.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,13 @@
 <template>
-  <b-container class="home">
-    <b-row fluid>
-      <b-col>
-      </b-col>
-    </b-row>
-    <b-row fluid>
-      <b-col>
+  <div class="container home">
+    <div class="row">
+      <div class="col">
         <p>One interpretation of the exercises for the <a href="https://www.udemy.com/course-dashboard-redirect/?course_id=1918908">D3.js course</a> by Shaun Pelling.</p>
         <p>Use the links above to navigate to the projects.</p>
-      </b-col>
-    </b-row>
+      </div>
+    </div>
     <project-preview-carousel />
-  </b-container>
+  </div>
 </template>
 
 <script>
