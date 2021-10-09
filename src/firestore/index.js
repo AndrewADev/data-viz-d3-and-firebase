8e05fcdc63dc837593564bd1c3de2c9bd8572210
Andrew A <andrewadev@gmail.com>
2021-10-09 21:51:36 +0200

    Migrate emulator setup to v9 API
    Make changes needed to connect to the local emulator for tests.
    
    Note that the settings are now passed as part of init, and `getFirestore()` is now `initializeFirestore()` with defaults/no settings specified.
    
    See here for more details: https://firebase.google.com/docs/reference/js/firestore_.md#initializefirestore

--- src/firestore/index.js
+++ src/firestore/index.js
@@ -1,17 +1,20 @@
-import { getFirestore } from 'firebase/firestore'
+import { getFirestore, connectFirestoreEmulator, initializeFirestore } from 'firebase/firestore'
 
 export const configureDatabase = (app) => {
-  const db = getFirestore(app)
+  let db
 
   if (process.env.VUE_APP_FIREBASE_BACKEND === 'emulator') {
     // eslint-disable-next-line no-console
     console.info('Connecting to Firebase emulator...')
+
     // Needed due to Cypress, in order to keep communication open to Firebase with
     // Cypress in the middle, per first comment here: https://stackoverflow.com/a/61619639
     // Documentation on setting available here:
     //   https://firebase.google.com/docs/reference/js/firebase.firestore.Settings#optional-experimentalautodetectlongpolling
-    db.settings({ experimentalAutoDetectLongPolling: true })
-    db.useEmulator('localhost', 8050)
+    db = initializeFirestore(app, { experimentalAutoDetectLongPolling: true })
+    connectFirestoreEmulator(db, 'localhost', 8050)
+  } else {
+    db = getFirestore(app)
   }
   return db
 }
