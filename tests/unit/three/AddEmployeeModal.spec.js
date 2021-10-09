0b492a23bff8995a466e4d551f5088c9caca255d
Andrew A <andrewadev@gmail.com>
2021-10-09 20:33:03 +0200

    Update tests for test library API changes
    Make some necessary changes to the tests for updated vue-test API

--- tests/unit/three/AddEmployeeModal.spec.js
+++ tests/unit/three/AddEmployeeModal.spec.js
@@ -1,14 +1,11 @@
-import { createLocalVue, mount } from '@vue/test-utils'
+import { mount } from '@vue/test-utils'
 import AddEmployeeModal from '@/components/three/AddEmployeeModal'
-
-const localVue = createLocalVue()
 
 describe('AddEmployeeModal', () => {
   // i.e. does validation instead of disappearing
   it('does not disappear on button click', () => {
     const wrapper = mount(AddEmployeeModal, {
       attachTo: document.body,
-      localVue,
       propsData: {
         static: true
       }
@@ -17,6 +14,6 @@ describe('AddEmployeeModal', () => {
     const button = wrapper.find('button')
     button.trigger('click')
     expect(wrapper.isVisible).toBeTruthy()
-    wrapper.destroy()
+    wrapper.unmount()
   })
 })
