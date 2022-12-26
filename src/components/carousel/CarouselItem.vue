692e67e3645ce3904f3d35644670a983de0cdd7d
Andrew A <andrewadev@gmail.com>
2022-12-26 14:04:26 +0100

    Add a CarouselItem component
    Split out duplicated logic for preview 'slides' to a dedicated component.

---
+++ src/components/carousel/CarouselItem.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="carousel-item" >
+    <img class="d-block w-100" :src="imgSrc" :alt="altText" />
+    <div class="carousel-caption d-none d-md-block">
+      <h5>{{ heading }}</h5>
+      <p>{{ description }}</p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue'
+
+defineProps({
+  heading: String,
+  description: String,
+  imgSrc: String,
+  altText: String
+})
+
+</script>
+
+<style lang="scss" scoped>
+  .carousel-caption {
+    background-color: #111;
+    border-radius: 5px;
+    opacity: 0.80;
+  }
+</style>
