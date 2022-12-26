8054bb79e90ad245f5733236d20f147961c8b3a3
Andrew A <andrewadev@gmail.com>
2022-12-26 18:53:25 +0100

    More practical linter settings
    At least now we have the ability to test small changes without it needing to be perfect before every test compile.
    
    Still allow these to stop the show in prod mode though.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,24 @@
+const isProd = process.env.NODE_ENV === 'production'
+const warnOnlyInDev = () => isProd ? 'error' : 'warn'
+
 module.exports = {
   root: true,
   env: {
     node: true
   },
-  'extends': [
+  extends: [
     'plugin:vue/essential',
     '@vue/standard'
   ],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-console': isProd ? 'error' : 'off',
+    'no-debugger': isProd ? 'error' : 'off',
+    // Warn only for dev for now, because errors will keep app from running, and that is too disruptive for some rules
+    'no-unused-vars': warnOnlyInDev(),
+    'no-multiple-empty-lines': warnOnlyInDev(),
+    'no-trailing-spaces': warnOnlyInDev(),
+    'comma-dangle': warnOnlyInDev(),
+    'eol-last': warnOnlyInDev()
   },
   parserOptions: {
     parser: 'babel-eslint'
