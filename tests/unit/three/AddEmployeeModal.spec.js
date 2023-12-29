aad9fc7999d4dc50d54bd30bc89d79ce82a73bf9
Andrew Armbruster <AndrewADev@gmail.com>
2023-12-29 16:48:51 -0500

    Update Vue Test Utils
    Update Vue Test Utils to a Vue 3 compatible version.
    
    There weren't many required changes, and the current changes have the tests passing.
    
    For complete instructions, see the migration guide: https://test-utils.vuejs.org/migration/

--- tests/unit/three/AddEmployeeModal.spec.js
+++ tests/unit/three/AddEmployeeModal.spec.js
@@ -1,16 +1,13 @@
-import { createLocalVue, mount } from '@vue/test-utils'
+import { mount } from '@vue/test-utils'
 import AddEmployeeModal from '@/components/three/AddEmployeeModal.vue'
 import { describe, expect, it } from 'vitest'
-
-const localVue = createLocalVue()
 
 describe('AddEmployeeModal', () => {
   // i.e. does validation instead of disappearing
   it('does not disappear on button click', () => {
     const wrapper = mount(AddEmployeeModal, {
       attachTo: document.body,
-      localVue,
-      propsData: {
+      props: {
         static: true
       }
     })
@@ -18,6 +15,6 @@ describe('AddEmployeeModal', () => {
     const button = wrapper.find('button')
     button.trigger('click')
     expect(wrapper.isVisible).toBeTruthy()
-    wrapper.destroy()
+    wrapper.unmount()
   })
 })
